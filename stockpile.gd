3fb004938d623b25f425333b16c3d9134bb213f9
Alyssa Carter <trillionalyssa@gmail.com>
2024-06-30 08:15:27 -0400

    Add textual label for stockpile >= 6

--- stockpile.gd
+++ stockpile.gd
@@ -4,9 +4,14 @@ extends Node2D
 var apples = 0:
 	set(value):
 		var i = 1
-		for child in get_children():
+		for child in $Contents.get_children():
 			if i <= value:
 				child.visible = true
 			else:
 				child.visible = false
 			i += 1
+		if value >= 6:
+			$Label.text = str(value)
+			$Label.visible = true
+		else:
+			$Label.visible = false
